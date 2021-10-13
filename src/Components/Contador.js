47a7d2a87badff23e47874c24296abac5a382913
Kaio <luizkaio9815@gmail.com>
2021-10-13 14:53:22 -0300

    Cria o style do Component Contador

--- src/Components/Contador.js
+++ src/Components/Contador.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './Contador.css'
 
 
 export const Contador = () => {
@@ -6,7 +7,7 @@ export const Contador = () => {
     const [number, setNumber] = useState(0);
 
     return(
-        <div>
+        <div className='container'>
 
             <div className="resultado">
                 <h1>{number}</h1>
